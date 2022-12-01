8c324d70886100d03daf37ea7d2c6db3693eb059
Garrett Cochran <garrettjcochran@gmail.com>
2022-12-01 15:16:31 -0700

    12.01.22 #1

---
+++ ruby_codewars/6_kyu/count_characters_in_your_string.rb
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/ruby
+
+# problem:
+# The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+# What if the string is empty? Then the result should be empty object literal, {}.
+
+def count_chars(s)
+  result = {}
+  default = 1
+  s.each_char do |char|
+    result.keys.include?(char) ? result[char] += 1 : result[char] = default
+    end
+  result
+end
+
+p count_chars('aba')
+p count_chars('')
+p count_chars('abcdefabcdeabcab')
